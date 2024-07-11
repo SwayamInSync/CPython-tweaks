b5980042d92b93ad0d81ed67bf5886141fdd615c
Swayam <74960567+SwayamInSync@users.noreply.github.com>
2024-07-11 22:09:46 +0530

    fix absolute value threshold

--- mandelbrot_set.c
+++ mandelbrot_set.c
@@ -5,46 +5,44 @@ Range in real line is [-2, 0.25]
 
 
 #include <stdio.h>
+#include <math.h>
 
-#define WIDTH 80
+#define WIDTH 100
 #define HEIGHT 80
 #define THREHSOLD 4.0
 #define MAX_ITER 1000
 
+double get_abs_value(double r, double i)
+{
+    return sqrt(r*r + i*i);
+}
 
-int mandelbrot(double cr, double ci) {
-    double zr = 0.0, zi = 0.0;
-    double zr2, zi2;
-    int n;
-    for (n = 0; n < MAX_ITER; n++) {
-        zr2 = zr * zr;
-        zi2 = zi * zi;
-        if (zr2 + zi2 > THREHSOLD)
+int mandelbrot(double cr, double ci) 
+{
+    double z_real = 0.0, z_img = 0.0;
+    double z_real2, z_img2;
+
+    for (int n = 0; n < MAX_ITER; n++) {
+        z_real2 = z_real * z_real;
+        z_img2 = z_img * z_img;
+
+        if (get_abs_value(z_real2, z_img2) > THREHSOLD)
             return n;
-        zi = 2.0 * zr * zi + ci;
-        zr = zr2 - zi2 + cr;
+
+        z_img = 2.0 * z_real * z_img + ci;
+        z_real = z_real2 - z_img2 + cr;
     }
     return MAX_ITER;
 }
 
 int main() {
-    for (int y = 0; y < HEIGHT; y++) {
+    for (int y = 0; y < HEIGHT; y++) 
+    {
         for (int x = 0; x < WIDTH; x++) {
             double cr = (x - WIDTH/2.0) * 4.0 / WIDTH; // linear transformation to keeping in the range
             double ci = (y - HEIGHT/2.0) * 4.0 / HEIGHT;
             
             int value = mandelbrot(cr, ci);
-            
-            // if (value == MAX_ITER)
-            //     putchar(' ');
-            // else if (value > MAX_ITER/2)
-            //     putchar('.');
-            // else if (value > MAX_ITER/4)
-            //     putchar('*');
-            // else if (value > MAX_ITER/8)
-            //     putchar('+');
-            // else
-            //     putchar('#');
 
             if(value == MAX_ITER)
                 printf("*");
